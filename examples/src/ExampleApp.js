3dc1113d24c6209e404b42dfb1407562c9cb6846
amsul <reach@amsul.ca>
2017-12-11 22:57:04 -0500

    Improve the reporting

--- examples/src/ExampleApp.js
+++ examples/src/ExampleApp.js
@@ -24,25 +24,65 @@ class CustomComponent extends React.Component {
   }
 }
 
+class AnotherCustomComponent extends React.Component {
+  render() {
+    const {
+      onPressStartStop,
+      onPressWasted,
+      onPressUseful,
+      Br,
+      Button,
+      View,
+    } = this.props
+    return (
+      <View>
+        <Button onClick={onPressStartStop}>Start/stop</Button>
+        <Br /><Br />
+        <Button onClick={onPressWasted}>Render wasted</Button>
+        <Br /><Br />
+        <Button onClick={onPressUseful}>Render useful</Button>
+      </View>
+    )
+  }
+}
+
 const MeasuredComponent = measure({
   isCollapsed: false,
   getId: 'example',
   Component: CustomComponent,
 })
 
+const AnotherMeasuredComponent = measure({
+  isCollapsed: false,
+  getId: 'example-2',
+  Component: AnotherCustomComponent,
+})
+
 export default class ExampleApp extends React.Component {
 
   state = {}
   render() {
+    const { Br } = this.props
     return (
-      <MeasuredComponent
-        {...this.props}
-        ref={node => this.measuredComponent = node}
-        useful={this.state.useful}
-        onPressStartStop={() => this.onPressStartStop()}
-        onPressWasted={() => this.setState({ wasted: !this.state.wasted })}
-        onPressUseful={() => this.setState({ useful: !this.state.useful })}
-        />
+      <React.Fragment>
+        <MeasuredComponent
+          {...this.props}
+          ref={node => this.measuredComponent = node}
+          useful={this.state.useful}
+          onPressStartStop={() => this.onPressStartStop()}
+          onPressWasted={() => this.setState({ wasted: !this.state.wasted })}
+          onPressUseful={() => this.setState({ useful: !this.state.useful })}
+          />
+        <Br /><Br /><Br />
+        <AnotherMeasuredComponent
+          {...this.props}
+          ref={node => this.measuredComponent = node}
+          useful={this.state.useful}
+          onPressStartStop={() => this.onPressStartStop()}
+          onPressWasted={() => this.setState({ wasted: !this.state.wasted })}
+          onPressUseful={() => this.setState({ useful: !this.state.useful })}
+          />
+      </React.Fragment>
     )
   }
 
